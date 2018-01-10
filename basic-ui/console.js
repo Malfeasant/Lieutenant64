a3222758188d64b7c54906f51f22de027909377e
Malfeasant <mischasumrall@gmail.com>
2018-01-09 22:08:16 -0700

    Some tweaks
    Might need to look at other css libraries... mini doesn't do hiding all
    that easily.

--- basic-ui/console.js
+++ basic-ui/console.js
@@ -5,12 +5,14 @@ function init() {
   var context = canvas.getContext('2d');
   canvas.width = "384";
   canvas.height = "216";
-  context.fillStyle = "#000000";
+  context.fillStyle = "#3f7fff";
   context.fillRect(0,0,canvas.width,canvas.height);
+  context.fillStyle = "#0000ff";
+  context.fillRect(4*8,1*8,40*8,25*8);
   window.addEventListener('resize', resizeCanvas, false);
   resizeCanvas();
 }
 
 function resizeCanvas() {
-  canvas.style.height = (canvas.clientWidth * .7) + 'px';
+  canvas.style.height = (canvas.clientWidth * .75) + 'px';
 }
