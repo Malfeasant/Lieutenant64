4ad33130d5861c315058b91f8c59ef7c3b449290
Malfeasant <mischasumrall@gmail.com>
2018-01-09 23:44:33 -0700

    Somewhat promising...
    But would be nice on wide screen to have menus always out without
    covering the display...

--- basic-ui/console.js
+++ basic-ui/console.js
@@ -16,3 +16,19 @@ function init() {
 function resizeCanvas() {
   canvas.style.height = (canvas.clientWidth * .75) + 'px';
 }
+
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
