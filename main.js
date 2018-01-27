80ac263e430e7d42247c9e7fac48fd84814c4cfc
Malfeasant <mischasumrall@gmail.com>
2018-01-26 19:50:26 -0700

    Bare bones

---
+++ main.js
@@ -0,0 +1,19 @@
+var pageBits = {};
+
+function init() {
+  pageBits.canvas = document.getElementById("screen");
+  pageBits.canvas.width = 384;
+  pageBits.canvas.height = 216;
+  pageBits.context = pageBits.canvas.getContext("2d");
+  window.addEventListener('resize', resizeCanvas, false);
+  resizeCanvas();
+  // test pattern (just to make the canvas show up)
+  pageBits.context.fillStyle = "#3f7fff";
+  pageBits.context.fillRect(0,0,pageBits.canvas.width, pageBits.canvas.height);
+  pageBits.context.fillStyle = "#0000ff";
+  pageBits.context.fillRect(4*8,1*8,40*8,25*8);
+}
+
+function resizeCanvas() {
+  pageBits.canvas.style.height = (pageBits.canvas.clientWidth * .75) + 'px';
+}
