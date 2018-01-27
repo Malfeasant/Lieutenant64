98edc496688e5b3c76d7a2eba4bb9edaa576bce5
Malfeasant <mischasumrall@gmail.com>
2018-01-27 16:14:25 -0700

    Why?

--- Control/control.js
+++ Control/control.js
@@ -4,24 +4,46 @@ var control = (function() {
   var mode;
 
   function step() {
-    if (mode == "Step") {
-      // do a step
-    } else {
+    if (mode !== "Step") {
       mode = "Step";
+    } else {
+      // do a step
     }
   }
 
   function play() {
     if (mode !== "Play") {
       // setup for realtime
+      mode = "Play";
     }
   }
 
   function fast() {
     if (mode !== "Fast") {
       // setup for unlimited
+      mode = "Fast";
     }
   }
 
-  return { step: step, play: play, fast: fast };
+  var perf = {
+    cycles: 0,
+    millis: 0,
+    lastFrame: null
+  };
+  function doCycle() {
+    perf.cycles++;
+  }
+
+  function doFrame(timeNow) {
+    if (perf.lastFrame) {
+      millis += timeNow - perf.lastFrame;
+    }
+    perf.lastFrame = timeNow;
+    if (imageData && exports.showImage) exports.showImage(imageData);
+    requestAnimationFrame(doFrame);
+  }
+  requestAnimationFrame(doFrame);
+
+  var exports = { step: step, play: play, fast: fast };
+  return exports;
 })();
